7012c238090c0c844791c254188d196be3af197c
cakeinpanic <cakeinpanic@gmail.com>
2020-11-20 22:04:37 +0200

    disable button if name is not input

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,27 @@ import './index.scss';
 const startButton = document.getElementById('startButton');
 const startScreen = document.getElementById('start');
 const gameScreen = document.getElementById('game');
+const nameInput = document.getElementById('nameInput');
 
 const init = () => {
   gameScreen.style.display = 'none';
+  startButton.setAttribute('disabled', true);
+
+  nameInput.oninput = () => {
+    if (nameInput.value) {
+      startButton.removeAttribute('disabled');
+    } else {
+      startButton.setAttribute('disabled', true);
+    }
+  };
+  startButton.onclick = () => {
+    startGame(nameInput.value);
+  };
 };
 
-const startGame = () => {
+const startGame = (playerName: string) => {
   gameScreen.style.display = null;
   startScreen.style.display = 'none';
-};
-
-startButton.onclick = () => {
-  startGame();
 };
 
 init();
