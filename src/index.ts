c53eb224b5296500c5ffa09e40c4f93456aac33c
cakeinpanic <cakeinpanic@gmail.com>
2020-11-20 21:52:37 +0200

    remove button to input

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,17 @@ import './index.scss';
 
 import { a } from './a';
 
-a(125);
+const startbutton = document.getElementById('startButton');
+const startScreen = document.getElementById('start');
+const gameScreen = document.getElementById('game');
+
+const init = () => {
+  gameScreen.style.display = 'none';
+};
+
+startbutton.onclick = () => {
+  gameScreen.style.display = 'true';
+  startScreen.style.display = 'none';
+};
+
+init();
