db5a623b5f7c74b605ee2fe98af55712506dbf70
Калмаков Алексей <AOKalmakov@sberbank.ru>
2020-11-21 18:48:02 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -95,6 +95,6 @@ const startGame = async (playerName: string) => {
 
 window.onload = async () => {
   enableMobileConsole();
-  startSnakeGame();
-  //   await initApp();
+  //   startSnakeGame();
+  await initApp();
 };
