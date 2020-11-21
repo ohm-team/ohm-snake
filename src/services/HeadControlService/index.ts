bf690645da12f7c27ddf1d7967f72f92e052fc11
Alexander Gusman <alex@goooseman.ru>
2020-11-21 13:25:16 +0200

    fix: typing error after class refactor

--- src/services/HeadControlService/index.ts
+++ src/services/HeadControlService/index.ts
@@ -3,7 +3,7 @@ import HeadControlService from './HeadControlService';
 export { default } from './HeadControlService';
 
 export const initHeadControl = () => {
-  const headControlService = new HeadControlService('head-preview', { disableRestPosition: false });
+  const headControlService = new HeadControlService('head-preview', {});
   headControlService.toggle(true);
   headControlService.addEventListener('mouse opened', () => console.log('mouse opened'));
   headControlService.addEventListener('mouse closed', () => console.log('mouse closed'));
