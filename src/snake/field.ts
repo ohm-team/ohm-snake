b7cdf45eb00a97246461a73c4c54b11203194206
Alexander Gusman <alex@goooseman.ru>
2020-11-22 00:20:11 +0200

    design(snake): design the game field

--- src/snake/field.ts
+++ src/snake/field.ts
@@ -4,11 +4,13 @@ import { HALF_PI } from './constants';
 import { dashline, flat_blue } from './materials';
 
 export const buildField = (scene: THREE.Scene, size: FieldSize) => {
+  const fieldHeight = size.unitSize * 2;
   const floor = new THREE.Mesh(new THREE.PlaneBufferGeometry(size.x, size.y, 0), flat_blue);
   floor.name = 'FloorPlane';
   floor.rotateX(-HALF_PI);
 
-  const lineSegments = new THREE.LineSegments(new THREE.EdgesGeometry(new THREE.BoxBufferGeometry(size.x, size.unitSize, size.y)), dashline);
+  const lineSegments = new THREE.LineSegments(new THREE.EdgesGeometry(new THREE.BoxBufferGeometry(size.x, fieldHeight, size.y)), dashline);
+  lineSegments.position.set(0, fieldHeight / 2, 0);
   lineSegments.name = 'BoardOutline';
   lineSegments.computeLineDistances();
 
